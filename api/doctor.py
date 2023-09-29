1b76db360ba042eeffcbf3e81f5d5fe1b632fba5
hopbv <vanhop3499@gmail.com>
2023-09-29 13:17:47 +0700

    update doctor with find drug in storage

--- api/doctor.py
+++ api/doctor.py
@@ -1,4 +1,5 @@
 import glob
+import re
 from dotenv import load_dotenv
 load_dotenv()
 import os
@@ -8,6 +9,7 @@ from pydantic import BaseModel
 from llama_index import (QuestionAnswerPrompt, RefinePrompt)
 from store_db_index import build_index
 from langchain.chat_models import ChatOpenAI
+from store_mongodb import StorageMongoDB
 
 temperature = 0 # It can be range from (0-1) as openai
 max_tokens = 512 # token limit
@@ -27,10 +29,15 @@ QA_PROMPT = QuestionAnswerPrompt(PROMPT_TEMPLATE)
 
 # Build index and query engine
 query_engine = index.as_query_engine(text_qa_template=QA_PROMPT,llm=ChatOpenAI(temperature=temperature,model="gpt-3.5-turbo",max_tokens=max_tokens))
+storage_db = StorageMongoDB(server=os.environ['MONGODB_SERVER'], port=int(os.environ['MONGODB_PORT']), db=os.environ['MONGODB_DB'])
+
 app = FastAPI()
 
 class PatientDescription(BaseModel):
     summary: str
+    
+class Prescription(BaseModel):
+    text: str
 
 @app.post("/doctor")
 def send_to_doctor(description: PatientDescription):
@@ -43,3 +50,47 @@ def send_to_doctor(description: PatientDescription):
             "response": response
         }
     }
+    
+
+def get_actives(prescription):
+    all_active = []
+    for active in storage_db.actives:
+        if active == None:
+            continue
+        if active in prescription:
+            x = re.search(active, prescription)
+            all_active.append({
+                'active': active, 
+                'start': x.start()
+            })
+    
+    all_active.sort(key=lambda x: x['start'])
+
+    return all_active
+    
+@app.get("/storage")
+def get_drugs(prescription: Prescription):
+    print(prescription.text)
+    all_actives = get_actives(prescription.text)
+    all_drugs = []
+    
+    for active in all_actives:
+        active_name = active['active']
+        drugs = list(storage_db.find_drug(active_name, 3, -1))
+        for idx, drug in enumerate(drugs):
+            drugs[idx]['_id'] = str(drug['_id'])
+        all_drugs.append({
+            'active': active_name,
+            'drugs': drugs
+        })
+    
+    print('check', all_drugs)
+    
+    return {
+        "msg": "SUCCESS",
+        "code": 0,
+        "data": {
+            "response": all_drugs
+        }
+    }
+    
---
+++ store_mongodb.py
@@ -0,0 +1,80 @@
+import pymongo
+import json
+import re
+
+class StorageMongoDB:
+    def __init__(self, server, port, db):
+        connection = pymongo.MongoClient(
+            server,
+            port
+        )
+        self.db = connection[db]
+        
+        self._actives = self.db['drug'].distinct('Hoạt chất')
+        
+    def insert_one(self, collection_name, item):
+        collection = self.db[collection_name]
+        collection.insert_one(item)
+        
+    def insert_many(self, collection_name, items):
+        collection = self.db[collection_name]
+        collection.insert_many(items)
+        
+    def find_drug(self, active_name, limit=3, sort=-1):
+        collection = self.db['drug']
+        items = collection.find({'Hoạt chất': active_name}).sort('Số lượng', sort).limit(limit)
+        return items
+    
+    @property
+    def actives(self):
+        return self._actives
+        
+def push_collections():
+    MONGODB_SERVER = "localhost"
+    MONGODB_PORT = 27017
+    MONGODB_DB = "storage"
+    
+    db = StorageMongoDB(MONGODB_SERVER, MONGODB_PORT, MONGODB_DB)
+    
+    with open("data/storage/drug_filtered.json") as f:
+        drug_items = json.load(f)
+    db.insert_many("drug", drug_items)
+    
+    with open("data/storage/add_filterd.json") as f:
+        add_items = json.load(f)
+    db.insert_many("add", add_items)
+        
+if __name__ == '__main__':
+    push_collections()
+    
+    # MONGODB_SERVER = "localhost"
+    # MONGODB_PORT = 27017
+    # MONGODB_DB = "storage"
+    
+    # db = StorageMongoDB(MONGODB_SERVER, MONGODB_PORT, MONGODB_DB)
+    
+    # # drugs = db.find_drug('Ibuprofen')
+    
+    # # for drug in drugs:
+    # #     print(drug)
+    
+    # # print(db.actives)
+    
+    # text = "Đơn thuốc tham khảo:\n\n1. Paracetamol: Dùng để giảm sốt và giảm đau.\n   Liều lượng: Uống 1-2 viên mỗi 4-6 giờ khi cần thiết. Không vượt quá 8 viên trong 24 giờ.\n\n2. Ibuprofen: Dùng để giảm đau và giảm viêm.\n   Liều lượng: Uống 1-2 viên mỗi 6-8 giờ khi cần thiết. Không vượt quá 6 viên trong 24 giờ.\n\n3. Chlorpheniramine: Dùng để giảm triệu chứng dị ứng như chảy nước mũi và ngứa.\n   Liều lượng: Uống 1-2 viên mỗi 4-6 giờ khi cần thiết. Không vượt quá 6 viên trong 24 giờ.\n\n4. Dextromethorphan: Dùng để giảm ho.\n   Liều lượng: Uống 1-2 viên mỗi 4-6 giờ khi cần thiết. Không vượt quá 8 viên trong 24 giờ.\n\nLưu ý: Đây chỉ là đơn thuốc tham khảo và không thay thế cho sự tư vấn và chỉ định của bác sĩ. Trước khi sử dụng bất kỳ loại thuốc nào, hãy tham khảo ý kiến ​​của bác sĩ hoặc nhà dược."
+    
+    # all_active = []
+    
+    # for active in db.actives:
+    #     if active == None:
+    #         continue
+    #     if active in text:
+    #         x = re.search(active, text)
+    #         all_active.append({
+    #             'active': active, 
+    #             'start': x.start()
+    #         })
+    #         print(x.start())
+    
+    # all_active.sort(key=lambda x: x['start'])
+
+    # print(all_active)
